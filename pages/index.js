92fd3e6b44f1dfd70ed897c7836316c9f46c7a06
arnaudjnn <hello@grimp.io>
2019-11-08 13:51:58 +0100

    refacto index

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,51 @@
+import React, { Component } from 'react';
 import Head from 'next/head'
 import TagManager from 'react-gtm-module'
 import Header from "../components/Header"
 import Heading from "../components/Heading"
 
-// Google Tag Manager
 const tagManagerArgs = {
   gtmId: 'GTM-MQ2QHK6'
 }
-TagManager.initialize(tagManagerArgs)
-// End Google Tag Manager
 
-export default () => (
-  <div>
-    <Head>
-      <title>Grimp - Call-To-Action manager for busy marketers</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
-    <style jsx global>{`
-      body { 
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        -webkit-font-smoothing: antialiased;
-        line-height: 1.3;
-      }
-      h1 {
-        font-size: 46px;
-      }
-      h4 {
-        font-size: 22px;
-        letter-spacing: -0.48px;
-        margin-bottom: 30px;
-        line-height: 1.6em;
-        color: rgba(23, 22, 24, 0.8);
-        font-weight: normal;
-      }
-    `}</style>
-    
-    <Header />
-    <Heading />
-  </div>
-)
+class App extends Component {
+  componentDidMount () {
+    TagManager.initialize(tagManagerArgs)
+  }
+
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Grimp - Call-To-Action manager for busy marketers</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <style jsx global>{`
+          body { 
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            -webkit-font-smoothing: antialiased;
+            line-height: 1.3;
+          }
+          h1 {
+            font-size: 46px;
+          }
+          h4 {
+            font-size: 22px;
+            letter-spacing: -0.48px;
+            margin-bottom: 30px;
+            line-height: 1.6em;
+            color: rgba(23, 22, 24, 0.8);
+            font-weight: normal;
+          }
+        `}</style>
+        
+        <Header />
+        <Heading />
+      </div>
+    );
+  }
+}
+
+export default App
+
+
