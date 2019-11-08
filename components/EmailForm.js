27551b099ae6a48ae8d06ee697e944473b474997
arnaudjnn <hello@grimp.io>
2019-11-08 17:22:01 +0100

    form hubspot

--- components/EmailForm.js
+++ components/EmailForm.js
@@ -1,21 +1,11 @@
-import {
-  Form,
-  Select,
-  Input,
-  Button
-} from 'antd'
-
-const FormItem = Form.Item
+import {Form, Input} from 'antd'
 
 const EmailForm= () => (
   <div style={{ marginTop: 100 }}>
-  <form>
-    <label>
-      Name:
-      <input type="email" value="arnaud@gmail.com" />
-    </label>
-    <input type="submit" value="Submit" />
-  </form>
+    <Form layout='inline'>
+      <Input type='email' size='large' placeholder="Type in your e-mail address..." required style={{ width: 250 }} />
+      <input type="submit" class="ant-btn ant-btn-primary ant-btn-lg" value="Request access" />
+    </Form>
   </div>
 )
 
