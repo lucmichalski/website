0007df384c002cdeb6cac4d239aa30e64909b84e
arnaudjnn <hello@grimp.io>
2019-11-08 16:29:57 +0100

    test html form

--- components/EmailForm.js
+++ components/EmailForm.js
@@ -10,6 +10,10 @@ const FormItem = Form.Item
 const EmailForm= () => (
   <div style={{ marginTop: 100 }}>
     <Form layout='inline'>
+    <form>
+      <input type="email">hello</input>
+      <input type="submit">Btn</input>
+    </form>
       <FormItem>
         <Input size='large' htmlType='email' placeholder="Type in your e-mail address..." style={{ width: 250 }} />
       </FormItem>
--- components/Heading.js
+++ components/Heading.js
@@ -10,7 +10,7 @@ const Heading = () => (
   <div style={{paddingTop: "120px"}}>
     <Col span={12} offset={6}>
       <BodyWrapper>
-        <h1>Enpower content startegy conversion</h1>
+        <h1>Enpower content strategy conversions</h1>
         <h4>The all-in-one tool dedicated to content-centric marketers.
         Create, embed, test, and measure – with Grimp.</h4>
         <EmailForm />
