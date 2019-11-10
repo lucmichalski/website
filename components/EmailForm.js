836d75e50380650c8a03a4ffe6d7376e3332c773
arnaudjnn <hello@grimp.io>
2019-11-10 22:16:22 +0100

    formik

--- components/EmailForm.js
+++ components/EmailForm.js
@@ -1,87 +1,35 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useFormik } from 'formik';
 
-const EmailForm= () => {
-  const [status, setStatus] = useState({
-    submitted: false,
-    submitting: false,
-    info: { error: false, msg: null }
-  })
+const EmailForm = () => {
 
-  const [inputs, setInputs] = useState({
-    email: ''
-  })
-
-  const handleResponse = (status, msg) => {
-    if (status === 200) {
-      setStatus({
-        submitted: true,
-        submitting: false,
-        info: { error: false, msg: msg }
+  const formik = useFormik({
+    initialValues: {      
+      email: '',
+    },
+    onSubmit: async values => {
+      await fetch('/api/send', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
       })
-      setInputs({
-        email: ''
-      })
-    } else {
-      setStatus({
-        info: { error: true, msg: msg }
-      })
-    }
-  }
-
-  const handleOnChange = e => {
-    e.persist()
-    setInputs(prev => ({
-      ...prev,
-      [e.target.id]: e.target.value
-    }))
-    setStatus({
-      submitted: false,
-      submitting: false,
-      info: { error: false, msg: null }
-    })
-  }
-
-  const handleOnSubmit = async e => {
-    e.preventDefault()
-    setStatus(prevStatus => ({ ...prevStatus, submitting: true }))
-    const res = await fetch('/api/send', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(inputs)
-    })
-    const text = await res.text()
-    handleResponse(res.status, text)
-  }
+    },
+  });
 
   return (
-    <main>
-      <form onSubmit={handleOnSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="email"
-          onChange={handleOnChange}
-          required
-          value={inputs.email}
-        />
-        <button type="submit" disabled={status.submitting}>
-          {!status.submitting
-            ? !status.submitted
-              ? 'Submit'
-              : 'Submitted'
-            : 'Submitting...'}
-        </button>
-      </form>
-      {status.info.error && (
-        <div className="error">Error: {status.info.msg}</div>
-      )}
-      {!status.info.error && status.info.msg && (
-        <div className="success">{status.info.msg}</div>
-      )}
-    </main>
-  )
+    <form onSubmit={formik.onSubmit}>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default EmailForm
