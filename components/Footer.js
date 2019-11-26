5bfb84c795c0185a60a421dd1f19f472e28e4c09
arnaudjnn <hello@grimp.io>
2019-11-26 17:39:45 +0100

    metomic head

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
 
   render() {
     return (
-      <a href="javascript:window.Metomic('ConsentManager:show')">metomic.io cookies manager</a>
+      <a href="javascript:window.Metomic('ConsentManager:show')"/>
     )
       
   }
