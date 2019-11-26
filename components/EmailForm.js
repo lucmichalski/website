83cf4e5e9f9a1dde7272f93e16a568441af36b66
arnaudjnn <hello@grimp.io>
2019-11-26 15:56:01 +0100

    formik form

--- components/EmailForm.js
+++ components/EmailForm.js
@@ -1,35 +1,78 @@
-import React from 'react'
-import { useFormik } from 'formik';
+import React from 'react';
+import { Formik, Form, useField } from 'formik';
+import styled from '@emotion/styled';
+import * as Yup from 'yup';
 
-const EmailForm = () => {
+const StyledErrorMessage = styled.div`
+  font-size: 12px;
+  color: var(--red-600);
+  width: 400px;
+  margin-top: 0.25rem;
+  &:before {
+    content: "❌ ";
+    font-size: 10px;
+  }
+  @media (prefers-color-scheme: dark) {
+    color: var(--red-300);
+  }
+`;
 
-  const formik = useFormik({
-    initialValues: {      
-      email: '',
-    },
-    onSubmit: async values => {
-      await fetch('/api/send', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(values)
-      })
-    },
-  });
-
+const MyTextInput = ({ label, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input> and also replace ErrorMessage entirely.
+  const [field, meta] = useField(props);
   return (
-    <form onSubmit={formik.onSubmit}>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <StyledErrorMessage>{meta.error}</StyledErrorMessage>
+      ) : null}
+    </>
   );
-}
+};
+
+// And now we can use these
+const EmailForm = () => {
+  return (
+    <>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          acceptedTerms: false, // added for our checkbox
+          jobType: '', // added for our select
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('Required')
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            fetch('/api/send', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(values)
+            })
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form>
+          <MyTextInput
+            name="email"
+            type="email"
+            placeholder="Type in your e-mail address..."
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
 
 export default EmailForm
