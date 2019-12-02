70665438fab9394746a1d319eaa2118d39ba98d1
arnaudjnn <hello@grimp.io>
2019-12-02 14:32:59 +0100

    remove confetti

--- components/EmailForm.js
+++ components/EmailForm.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Row, Col } from 'antd';
 import { Formik, Form, useField } from 'formik';
 import styled from '@emotion/styled';
-import Confetti from 'react-confetti'
-import {useWindowSize} from 'react-use';
 import * as Yup from 'yup';
 
 const StyledInput = styled.input`
@@ -105,9 +103,7 @@ const EmailForm = () => {
         }}
       >
         {props => props.submitCount && props.isValid ? (
-          <>
-            <StyledSuccessMessage>Thank you! We'll be in touch soon</StyledSuccessMessage>
-          </>
+          <StyledSuccessMessage>Thank you! We'll be in touch soon</StyledSuccessMessage>
         ) : (
         <Form>
           <Col xs={24} md={13}>
