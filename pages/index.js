302e91dbfa6268a7b2c2ed75d5fd11d131295e39
arnaudjnn <hello@grimp.io>
2019-12-03 16:10:07 +0100

    favicon

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,10 @@ class App extends Component {
         <Head>
           <title>Grimp - Power to marketing teams</title>
           <meta name='description' content='Make marketing pages convert again'/>
-          <link rel='icon' type="image/x-icon" href='/favicon.ico' />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+          <link rel="manifest" href="/static/site.webmanifest" />
           <style>{`.async-hide { opacity: 0 !important}`}</style>
           <script
             dangerouslySetInnerHTML={{
