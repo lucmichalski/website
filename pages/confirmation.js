47f7094ddfb717d2ff5a0af61ddd60c4e287865c
arnaudjnn <hello@grimp.io>
2019-12-08 10:09:56 +0100

    simple export

--- pages/confirmation.js
+++ pages/confirmation.js
@@ -1,10 +1,8 @@
 import Page from '../layouts/main'
 
-export default function Confirmation() {
-  return (
-    <Page>
-      <h1>Confirmation</h1>
-      <p>Thanks!</p>
-    </Page>
-  );
-}
+export default () => (
+  <Page>
+    <h1>Confirmation</h1>
+    <p>Thanks!</p>
+  </Page>
+)
