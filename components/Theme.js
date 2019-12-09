d026b84e0140f906fe29ac69fb73b05fba3afc28
arnaudjnn <hello@grimp.io>
2019-12-09 13:52:44 +0100

    refacto theme

--- components/Theme.js
+++ components/Theme.js
@@ -1,3 +1,5 @@
+import { createGlobalStyle } from 'styled-components'
+
 export const lightTheme = {
   bg: {
     primary: '#eff0f5',
@@ -33,3 +35,44 @@ export const darkTheme = {
   },
   // ...
 }
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		text-rendering: optimizelegibility;
+		line-height: 1.3;
+		background-color: ${props => props.theme.bg.primary};
+	}
+	h1 {
+		color: ${props => props.theme.text.primary};
+		font-weight: 600;
+		font-size: 48px;
+		letter-spacing: -4px;
+		line-height: 1;
+		text-align: center;
+	}
+	@media (min-width: 768px) { 
+		h1 {
+			text-align: left;
+			font-size: 72px;
+		}
+	}
+	p {
+		color: #8F9292;
+		font-size: 24px;
+		font-weight: 400;
+		line-height: 1.4;
+		margin-bottom: 40px;
+		text-align: center;
+	}
+	@media (min-width: 768px) { 
+		p {
+			text-align: left;
+			font-size: 26px;
+		}
+	}
+	*:focus {
+		outline:none
+	}
+`
