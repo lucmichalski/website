193366ee28f05f9a0c1491e0aef7c1fca34e0215
arnaudjnn <hello@grimp.io>
2019-12-13 09:50:41 +0100

    translations folder

---
+++ translations/strings.ts
@@ -0,0 +1,22 @@
+import { Strings } from './types'
+
+const strings: Strings = {
+  en: {
+    about: 'About René Magritte',
+    painting: 'The Painting',
+    artist: 'The Artist',
+    notAPipe: 'This is not a pipe',
+    bio: `René François Ghislain Magritte (21 November 1898 – 15 August 1967) was a Belgian Surrealist artist. He became well known for creating a number of witty and thought-provoking images. Often depicting ordinary objects in an unusual context, his work is known for challenging observers' preconditioned perceptions of reality. His imagery has influenced pop art, minimalist art and conceptual art.`,
+    readMore: 'Read more on Wikipedia...'
+  },
+  fr: {
+    about: 'Sur René Magritte',
+    painting: 'La peinture',
+    artist: "L'Artiste",
+    notAPipe: "Ceci n'est pas une pipe",
+    bio: `René Magritte est un peintre surréaliste belge, né le 21 novembre 1898 à Lessines1 dans le Hainaut (Belgique) et mort à Schaerbeek le 15 août 1967.`,
+    readMore: 'Lire plus sur Wikipedia...'
+  }
+}
+
+export default strings
