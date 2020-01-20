a7bae380d0f7bfe980ce19bb83192ce4a40c80c7
arnaudjnn <arnaudjeannin@outlook.com>
2020-01-20 17:00:47 +0100

    strapi backend graphql

--- frontend/pages/[lang]/demo.tsx
+++ frontend/pages/[lang]/demo.tsx
@@ -1,12 +1,13 @@
 import { Row, Col } from 'antd';
 import MainLayout from '../../layouts/Landing'
+import Nav from '../../components/nav'
 
 export default () => (
   <MainLayout>
     <Row type="flex" justify="center" align="middle" >
       <Col xs={24} md={10}>
         <h1>Demo</h1>
-        <img src="/widget1.png" width="600px" alt="Widget Grimp"/>
+        <Nav />
       </Col>
     </Row>
   </MainLayout>
