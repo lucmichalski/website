df7b0e04c429e65bbe370ffd9edcf3b9d628a867
arnaudjnn <arnaudjeannin@outlook.com>
2020-02-05 06:57:27 +0100

    apply page

--- pages/offers/[id]/apply.tsx
+++ pages/offers/[id]/apply.tsx
@@ -4,42 +4,27 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch';
 import parser from 'fast-xml-parser'
-import PageLayout from '../../layouts/Page'
+import PageLayout from '../../../layouts/Page'
 
-const OfferGroup = styled.div`
-  p {
-    text-align: justify;
-  }
-`
 
-const Offer = props => {
+const Apply = props => {
+
   const router = useRouter()
   const { id } = router.query
   const offer = props.offers.find(offer => offer.offer_keyid === Number(id))
 
   return (
-    <PageLayout title={offer.job_title}>
+    <PageLayout title="hello">
       <Row type="flex" justify="center" align="top">
-        <Col xs={24} md={6}>
-          <Link href="/offers/[id]/apply" as={`/offers/${offer.offer_keyid}/apply`}>
-            <a>Button</a>
-          </Link>
-        </Col>
         <Col xs={24} md={12}>
-          <OfferGroup>
-            <h2>À propos</h2>
-            <p>{offer.company_description}</p>
-            <h2>Descriptif du poste</h2>
-            <p>{offer.job_description}</p>
-          </OfferGroup>
+          {offer.job_title}
         </Col>
-        <Col xs={0} md={6}/>
       </Row>
     </PageLayout>
   )
 }
 
-Offer.getInitialProps = async function() {
+Apply.getInitialProps = async function() {
  
   const res = await fetch('http://www.mytalentplug.com/xml.aspx?jbID=u/S3BRjmcl8=')
   const xml = await res.text()
@@ -51,4 +36,5 @@ Offer.getInitialProps = async function() {
 
 };
   
-export default Offer
+  
+export default Apply
