677aeb6ca609d0556a9043bd15e4a9649f6fe2bb
arnaudjnn <arnaudjeannin@outlook.com>
2020-02-06 18:40:54 +0100

    appbase env

--- next.config.js
+++ next.config.js
@@ -39,7 +39,9 @@ module.exports = withLess(withFonts({
     }
     return config
   },
-  // env: {
-  //   EXAMPLE: process.env.EXAMPLE
-  // }
+  env: {
+    APPBASE_APP_ID: process.env.APPBASE_APP_ID,
+    APPBASE_API_URL: process.env.APPBASE_API_URL,
+    APPBASE_API_KEY: process.env.APPBASE_API_KEY
+  }
 }))
