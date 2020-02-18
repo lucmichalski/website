247bfa426c1b67092402dfab6fe65195ae958958
arnaudjnn <arnaudjeannin@outlook.com>
2020-02-18 13:47:31 +0100

    main meta

--- next-seo.config.js
+++ next-seo.config.js
@@ -11,9 +11,9 @@ export default {
     description: "Kernn est une démonstration concrète des solutions que nous proposons à nos clients afin de développer leur marque employeur et d'optimiser les performances.",
     images: [
       {
-        url: "url('/og-image.png')",
-        width: 1232,
-        height: 632,
+        url: "https://res.cloudinary.com/angulaire/image/upload/c_scale,h_630,w_1200/sample.jpg",
+        width: 1200,
+        height: 630,
         alt: 'Kernn Meta',
       }
     ]
